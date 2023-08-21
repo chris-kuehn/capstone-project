0a4a6e358e70fe65af4c88ab6a04cda9bca8eaa4
Christian Kühn <christian.kuhn.istanbul@gmail.com>
2023-08-21 17:30:00 +0200

    change componentname

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import ArtGenerator from "@/components/ArtGenerator";
-import CanvasOne from "@/components/CanvasOne";
+import Canvas from "@/components/Canvas";
 import Wrapper from "@/components/Wrapper";
 
 export default function HomePage() {
   return (
     <Wrapper>
-      <CanvasOne />
+      <Canvas />
     </Wrapper>
   );
 }
