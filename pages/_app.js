b69ea945090435b850b1b3f469940cb5a3746071
Christian Kühn <christian.kuhn.istanbul@gmail.com>
2023-08-31 23:26:11 +0200

    create artworks in gallery

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import GlobalStyle from "../styles";
 import { Roboto } from "@next/font/google";
+import { useState } from "react";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -7,10 +8,40 @@ const roboto = Roboto({
 });
 
 export default function App({ Component, pageProps }) {
+  const [listOfArtworks, setListofArtworks] = useState([
+    [
+      { color: 148 },
+      { rotation: 45 },
+      { outline: 0 },
+      {
+        lineWidthes: [2, 2, 2, 2, 2, 2, 2, 2],
+      },
+      { key: 1 },
+    ],
+    [
+      { color: 148 },
+      { rotation: 45 },
+      { outline: 0 },
+      {
+        lineWidthes: [2, 2, 2, 2, 2, 2, 2, 2],
+      },
+      { key: 2 },
+    ],
+    [
+      { color: 148 },
+      { rotation: 45 },
+      { outline: 0 },
+      {
+        lineWidthes: [2, 2, 2, 2, 2, 2, 2, 2],
+      },
+      { key: 3 },
+    ],
+  ]);
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} listOfArtworks={listOfArtworks} />
     </>
   );
 }
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,17 +1,26 @@
+import { useRef, useState } from "react";
 import GalleryCanvas from "@/components/GalleryCanvas";
 import Navigation from "@/components/Navigation";
 import Section from "@/components/Section";
 import SectionHeadline from "@/components/SectionHeadline";
 import GalleryHeader from "@/components/GalleryHeader";
+import Canvas from "@/components/Canvas";
 
-export default function HomePage() {
+export default function HomePage({ listOfArtworks }) {
   return (
     <>
       <Navigation pageStatus="gallery" />
       <GalleryHeader />
       <Section>
         <SectionHeadline> Artwork ID-1</SectionHeadline>
-        <GalleryCanvas />
+        {listOfArtworks.map((artwork) => (
+          <Canvas
+            key={artwork.key}
+            canvasParameter={artwork}
+            width="300"
+            height="300"
+          />
+        ))}
       </Section>
     </>
   );
